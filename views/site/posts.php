cee62b72fbea75ed979034b5ecf389d0d4a3c350
Dima <m305907@alunos.ipb.pt>
2020-05-04 08:54:27 +0100

    Posts responsive page, empty news view. (Previous commit didn't include them, idk why)

---
+++ views/site/posts.php
@@ -0,0 +1,175 @@
+<?php
+$this->title = 'Login';
+?>
+<div class="row">
+    <div class="input-group mx-auto mb-2">
+        <input type="text" class="form-control" placeholder="Search for a related">
+        <div class="input-group-append">
+            <button class="btn btn-secondary" type="button">
+                <i class="fas fa-search fa-sm"></i>
+            </button>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="row">
+
+            <div class="lang ml-auto">
+                <i class="devicon-python-plain"></i>
+                <p>Python</p>
+            </div>
+
+            <div class="lang">
+                <i class="devicon-csharp-plain"></i>
+                <p>C sharp</p>
+            </div>
+            <div class="lang">
+                <i class="devicon-go-plain"></i>
+                <p>GoLang</p>
+            </div>
+            <div class="lang mr-auto">
+                <i class="devicon-javascript-plain"></i>
+                <p>JavaScript</p>
+            </div>
+        </div>
+        <div class="row row-2">
+            <div class="lang ml-auto">
+                <i class="devicon-c-plain"></i>
+                <p>C</p>
+            </div>
+            <div class="lang">
+                <i class="devicon-swift-plain"></i>
+                <p>Swift</p>
+            </div>
+            <div class="lang">
+                <i class="devicon-php-plain"></i>
+                <p>php</p>
+            </div>
+            <div class="lang mr-auto">
+                <i class="devicon-cplusplus-plain"></i>
+                <p>C++</p>
+            </div>
+        </div>
+        <div class="row row-3">
+            <div class="lang ml-auto">
+                <i class="devicon-ruby-plain"></i>
+                <p>Ruby</p>
+            </div>
+            <div class="lang">
+                <i class="devicon-typescript-plain"></i>
+                <p>TypeScript</p>
+            </div>
+            <div class="lang">
+                <i class="devicon-java-plain"></i>
+                <p>Java</p>
+            </div>
+            <div class="lang mr-auto">
+                <i class="devicon-mysql-plain"></i>
+                <p>SQL</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col-md-12 col-lg-6 ">
+        <div class="post mr-auto">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="first-line mt-1">
+                                    <span class='rank'>
+                                        12
+                                        <i class="fas fa-star "></i>
+                                    </span>
+                            <span class='topic'>
+                                        Logical Operators in Python
+                                    </span>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-1">
+                        <a href="">
+                            <div class="button mx-auto">
+                                <span class='check center'>Read</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mt-1">
+                    <div class="col-12">
+                        <div class="code-sample mx-auto">
+                                    <pre class="prettyprint">
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Generate data
+        int arraySize = 32768;
+        int data[] = new int[arraySize];
+                                    </pre>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a href='#' class='author'>by: Matt Damon</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 col-lg-6 md-second">
+        <div class="post ml-auto">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="first-line mt-1">
+                                    <span class='rank '>
+                                        12
+                                        <i class="fas fa-star "></i>
+                                    </span>
+
+                            <span class='topic' data-toggle="tooltip" data-placement="top"
+                                  title="Logical Operators in Python">
+                                        Logical Operators in Python
+                                    </span>
+                        </div>
+                    </div>
+                    <div class="col-4 mt-1">
+                        <a href="">
+                            <div class="button mx-auto">
+                                <span class='check center'>Read</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mt-1">
+                    <div class="col-12">
+                        <div class="code-sample mx-auto">
+                                    <pre class="prettyprint">
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Generate data
+        int arraySize = 32768;
+        int data[] = new int[arraySize];
+                                    </pre>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a href='#' class='author'>by: Matt Damon</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
