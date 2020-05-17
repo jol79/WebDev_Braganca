74352134d90238a68ae20ce7fbc0556b18ae9d6b
Mykyta Chernov <m305908@alunos.ipb.pt>
2020-05-17 18:42:46 +0100

    1) Login page:     - changed common file in amnah extension, added new functionality and UI ( in the next commit size >992 px should be adapted for input fields (passwd/email));
    2) LoginAsset:
        - Imported useful css and js;
    
    3) LoginRegistrationReset css -> added;

--- assets/LoginAsset.php
+++ assets/LoginAsset.php
@@ -11,6 +11,10 @@ class LoginAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/LoginRegistrationReset_pages.css',
+        'css/bootstrap.css',
+        'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+        '//cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css',
 
     ];
     public $js = [
