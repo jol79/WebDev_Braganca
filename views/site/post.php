ea7279c8bba6769715e3ed56d20758f169664cd7
Corneliu Boboc <cartofica@users.noreply.github.com>
2020-05-18 15:13:42 +0100

    Added comment box on sample post

--- views/site/post.php
+++ views/site/post.php
@@ -160,6 +160,19 @@ print(os.getcwd())
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-12">
+            <form>
+                <div class="form-group">
+                    <label for="commentTextarea">Leave your comment:</label>
+                    <textarea class="form-control" id="coomentTextarea" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+
 </div>
 
 
