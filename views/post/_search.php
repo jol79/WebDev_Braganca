16b9257552df6edbd5d030eab0fa17742210e3cd
Dima <m305907@alunos.ipb.pt>
2020-05-20 08:58:49 +0100

    CRUD for 'post' and 'category', stored all classes of (progrm lang.) icons in a database.

---
+++ views/post/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Search\PostSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'post_id') ?>
+
+    <?= $form->field($model, 'date') ?>
+
+    <?= $form->field($model, 'topic') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'rating') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'user_id') ?>
+
+    <?php // echo $form->field($model, 'category_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
