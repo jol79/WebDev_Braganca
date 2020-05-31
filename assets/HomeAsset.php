eabe143c3fb477143e876151167651a76c9d3e2c
Mykyta Chernov <m305908@alunos.ipb.pt>
2020-05-31 15:35:13 +0100

    block elements on the home page have swapped with cube and text description of  pluses of the platform and how to use it;

--- assets/HomeAsset.php
+++ assets/HomeAsset.php
@@ -12,9 +12,11 @@ class HomeAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/home_page.css',
-        '//cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css'
+        '//cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css',
+
     ];
     public $js = [
+        'web/js/cube.js',
 
     ];
     public $depends = [
